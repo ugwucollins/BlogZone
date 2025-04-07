a6726023ed129b114d6803e18c9ac98936e7efe4
ugwucollins <123670768+ugwucollins@users.noreply.github.com>
2025-04-07 06:28:01 -0700

    Update Api.tsx
    updated the CORS baseURL

--- Frontend/src/Axios/Api.tsx
+++ Frontend/src/Axios/Api.tsx
@@ -3,8 +3,9 @@ import axios from "axios";
 // const Url = "https://mern-project-422f.vercel.app/api";
 const { VITE_REACT_CLOUD_NAME } = import.meta.env;
 
+  // baseURL: "https://vercel.com/movie-lands-projects/blog-zone-api/api/auth",
 const Api = axios.create({
-  baseURL: "https://vercel.com/movie-lands-projects/blog-zone-api/api/auth",
+  baseURL: "https://blog-zone-api.vercel.app/api/auth",
 });
 
 export default Api;
